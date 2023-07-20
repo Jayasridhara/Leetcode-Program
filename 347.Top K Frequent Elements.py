ec43dc314662284071022682f99c1fb900a481cd
jayasridhara <jayasridharasubramaniyan@gmail.com>
2023-07-20 10:41:09 +0530

    100+leetcode problem

--- 347.Top K Frequent Elements.py
+++ 347.Top K Frequent Elements.py
@@ -1,12 +1,18 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
-        for x in nums:
-            d[x] = d.get(x, 0) + 1
-        
-        lst = [(x, d[x]) for x in d]
-        lst.sort(reverse=True, key=lambda t:t[1])
-        return dict(lst[:k]).keys()
+
+def topKFrequent(a,k):
+    d = {}
+    for x in a:
+       d[x] = d.get(x, 0) + 1
+    print(d)
+    lst = [(x, d[x]) for x in d]
+    print(lst)
+    lst.sort(reverse=True, key=lambda t:t[1])
+    print(lst)
+    print(dict(lst[:k]).keys())
+    return dict(lst[:k]).keys()
+
+a= [3,2,2,2,1,1,1,1,4,4]
+topKFrequent(a,2)
 
 
         
